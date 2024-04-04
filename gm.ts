3412e890b1894f2bac4bed8439b1d51b6dc3b90e
Patrick Ching <burning.codex@gmail.com>
2024-04-04 07:37:32 -0600

    added multipull

--- gm.ts
+++ gm.ts
@@ -19,7 +19,24 @@ export async function pull(url_to_pull: string): Promise<string> {
 			},
 			onerror: async (r) => {
 				fail(`unable to pull ${url_to_pull}`);
-			},
+			}
 		});
 	});
 }
+
+export async function multi_pull(urls : string[])
+{
+	return await Promise.allSettled(urls.map((v, i, a) => {
+		return pull(v);
+	})).then((v) => {
+		return v.map((q, j, b) => {
+			return new Promise<string>((resolve, reject) => {
+				if (q.status === 'fulfilled') {
+					resolve(q.value);
+				} else {
+					reject(q.status + `-` + q.reason);
+				}
+			});
+		});
+	});
+};
---
+++ pull_test.ts
@@ -0,0 +1,11 @@
+import { multi_pull } from "./gm";
+
+multi_pull(["https://google.com", "https://reddit.com", "http://jasdlw.com"])
+	.then((v) => (v.forEach((v, i, a) => {
+		v.then((a) => {
+			const parser = new DOMParser();
+			let doc = parser.parseFromString(a, 'text/html');
+			console.log(doc);
+		}).catch(console.log);
+	})
+));

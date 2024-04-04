7cc9a9f9b532d8c9bd5c9caffbbcc877ca74ac28
Patrick Ching <burning.codex@gmail.com>
2024-04-04 10:07:35 -0600

    added headers to requests if needed

--- gm.ts
+++ gm.ts
@@ -5,15 +5,17 @@ declare function GM_xmlhttpRequest(a: to_use): void;
 interface to_use {
 	url: string;
 	method: string;
+	headers? : HeadersInit;
 	onload: (r: any) => void;
 	onerror: (r: any) => void;
 }
 
-export async function pull(url_to_pull: string): Promise<string> {
+export async function pull(url_to_pull: string, headers? : HeadersInit): Promise<string> {
 	return await new Promise((resolve, fail) => {
 		GM_xmlhttpRequest({
 			url: url_to_pull,
 			method: "GET",
+			headers : headers,
 			onload: async (r) => {
 				resolve(r.responseText);
 			},
@@ -24,19 +26,19 @@ export async function pull(url_to_pull: string): Promise<string> {
 	});
 }
 
-export async function multi_pull(urls : string[])
+export async function multi_pull(urls : string[], headers? : HeadersInit)
 {
-	return await Promise.allSettled(urls.map((v, i, a) => {
-		return pull(v);
-	})).then((v) => {
-		return v.map((q, j, b) => {
-			return new Promise<string>((resolve, reject) => {
-				if (q.status === 'fulfilled') {
-					resolve(q.value);
-				} else {
-					reject(q.status + `-` + q.reason);
-				}
-			});
+	const v_1 = await Promise.allSettled(urls.map(async (v, i, a) => {
+		return await pull(v, headers);
+	}));
+	return v_1.map((q, j, b) => {
+		return new Promise<string>((resolve, reject) => {
+			if (q.status === 'fulfilled') {
+				console.log(q.status + `-` + q.value);
+				resolve(q.value);
+			} else {
+				reject(q.status + `-` + q.reason);
+			}
 		});
 	});
 };

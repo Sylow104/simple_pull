c16c1358b400a5ae45e6fd57a7bc91221e1756bd
Patrick Ching <burning.codex@gmail.com>
2024-04-04 22:26:18 -0600

    updated test suite

--- pull_test.ts
+++ pull_test.ts
@@ -1,12 +1,11 @@
 import { multi_pull } from "./gm";
 
-multi_pull(["https://google.com", "https://reddit.com", "http://jasdlw.com"])
-	.then((v) => (v.forEach((v, i, a) => {
-		v.then((a) => {
+multi_pull(["https://google.com", "https://reddit.com", "http://jasdlw.com"],
+	(a) => {return a})
+	.then((v) => {v.forEach((q, j, b) => {
 			const parser = new DOMParser();
-			let doc = parser.parseFromString(a, 'text/html');
+			let doc = parser.parseFromString(q, 'text/html');
 			//console.log(`checking out - ${a}`);
 			console.log(doc);
-		}).catch(console.log);
-	})
-));
+		})})
+	.catch(console.log);
